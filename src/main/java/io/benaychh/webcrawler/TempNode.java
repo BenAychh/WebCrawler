35d200c156a3c5d66284554278ab682c09226840
BenAychh <ben.tomas.hernandez@gmail.com>
2016-06-16 10:17:44 -0600

    Added some getter methods

--- src/main/java/io/benaychh/webcrawler/TempNode.java
+++ src/main/java/io/benaychh/webcrawler/TempNode.java
@@ -5,11 +5,23 @@
  */
 package io.benaychh.webcrawler;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  *
  * @author benhernandez
  */
 public class TempNode implements Runnable {
+  /**
+   * The origin Node.
+   */
+  private final OriginNode origin;
   /**
    * The parent node of this node.
    */
@@ -20,11 +32,14 @@ public class TempNode implements Runnable {
   private final String path;
   /**
    * Simple constructor.
+   * @param pOrigin The origin node.
    * @param pParent the parent node (so we can add this once we have converted
    * it).
    * @param pPath the url this node represents.
    */
-  public TempNode(final Node pParent, final String pPath) {
+  public TempNode(final OriginNode pOrigin, final Node pParent,
+      final String pPath) {
+    this.origin = pOrigin;
     this.parent = pParent;
     this.path = pPath;
   }
@@ -32,11 +47,32 @@ public class TempNode implements Runnable {
    * Gets the url represented by this node.
    * @return the url of our node.
    */
-  public String getPath() {
+  public final String getPath() {
     return this.path;
   }
+  /**
+   * Gets the parent node.
+   * @return the parent node.
+   */
+  public final Node getParent() {
+    return this.parent;
+  }
   @Override
-  public void run() {
-   
+  public final void run() {
+    try {
+      Node newNode = this.parent.addChild(path);
+      // this path is in our website original url so we have to crawl it.
+      // We do this buy adding it to our executor service.
+      if (this.path.contains(origin.getPath())) {
+        Document page = Jsoup.connect(this.path).get();
+        Elements links = page.select("a[href]");
+        links.stream().forEach((link) -> {
+          this.origin.addToQueue(new TempNode(origin, newNode,
+              link.attr("abs:href")));
+        });
+      }
+    } catch (IOException ex) {
+      Logger.getLogger(TempNode.class.getName()).log(Level.SEVERE, null, ex);
+    }
   }
 }
