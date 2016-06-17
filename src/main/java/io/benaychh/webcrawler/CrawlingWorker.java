cefb92c3422a805a79ac05aeb6f597153120fbc5
BenAychh <ben.tomas.hernandez@gmail.com>
2016-06-17 15:14:17 -0600

    minor comment updates

--- src/main/java/io/benaychh/webcrawler/CrawlingWorker.java
+++ src/main/java/io/benaychh/webcrawler/CrawlingWorker.java
@@ -40,11 +40,15 @@ public class CrawlingWorker extends SwingWorker<Void, Void> {
   protected final Void doInBackground() throws Exception {
     this.ip.clearLog();
     // URL validation taken from http://www.santhoshreddymandadi.com/java/best-url-and-email-validation-using.html
-    String urlPattern = "^http(s{0,1})://[a-zA-Z0-9_/\\-\\.]+\\.([A-Za-z/]{2,5})[a-zA-Z0-9_/\\&\\?\\=\\-\\.\\~\\%]*";
+    String urlPattern = "^http(s{0,1})://[a-zA-Z0-9_/\\-\\.]+\\"
+        + ".([A-Za-z/]{2,5})[a-zA-Z0-9_/\\&\\?\\=\\-\\.\\~\\%]*";
+    // Check to see if this is a valid url.
     if (url.matches(urlPattern)) {
+      // Start the node.
       OriginNode on = new OriginNode(this.url, this.ip);
       ThreadPoolExecutor tpe = on.getThreadPool();
       final int sleepTime = 5000;
+      // Sit here until all of our tasks are complete.
       while (tpe.getTaskCount() != tpe.getCompletedTaskCount()) {
         try {
           Thread.sleep(sleepTime);
@@ -53,8 +57,10 @@ public class CrawlingWorker extends SwingWorker<Void, Void> {
               .log(Level.SEVERE, null, ex);
         }
       }
+      // End the thread pool.
       tpe.shutdown();
-      final int timeToFinish = 60;
+      final int timeToFinish = 10;
+      // Make sure everything ended ok.
       try {
         this.ip.setStatus("Finalizing", InfoPanel.Levels.ok);
         tpe.awaitTermination(timeToFinish, TimeUnit.SECONDS);
@@ -64,6 +70,7 @@ public class CrawlingWorker extends SwingWorker<Void, Void> {
       }
       this.ip.setStatus("Printing Tree", InfoPanel.Levels.warn);
       PrintWriter pw = new PrintWriter("tree.txt", "UTF-8");
+      // Start the tree printing, this is recursive.
       on.printTree(0, pw);
       this.ip.setStatus("Printing Finished", InfoPanel.Levels.ok);
       this.ip.appendLog("File located at: ");
